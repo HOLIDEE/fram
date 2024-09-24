faee990e9bb903e9a237fcf6fb6d6d0362083ac5
eneyret <manu@holidee.fr>
2024-09-24 17:10:09 +0200

    Buttons

--- src/main.ts
+++ src/main.ts
@@ -9,49 +9,22 @@ let currentPopup: any = undefined;
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
-		currentPopup = WA.ui.openPopup("welcomePopup", "Bienvenue au casting Fram & nxlvl \r \r La présentation se déroulera en visio principalement sur la terrasse \r \r Un membre de la Team va venir vous accueillir \r \r Sinon, pour vous rendre directement sur la terrasse, cliquez sur le premier bouton, pour une salle de casting individuel, cliquez sur un des boutons suivants :", [{
-			label: "TERRASSE",
-			className: "primary",
-			callback: () => {
-				WA.player.moveTo(1248, 672, 10);
-				}
-			},{
-			label: "CASTING 1",
-			className: "primary",
-			callback: () => {
-				WA.player.moveTo(352, 1344, 10);
-				}
-			},{
-			label: "CASTING 2",
-			className: "primary",
-			callback: () => {
-				WA.player.moveTo(512, 1344, 10);
-				}
-			},{
-			label: "<br>",
-			className: "primary",
-			callback: () => {
-				}
-			},{
-			label: "CASTING 3",
-			className: "primary",
-			callback: () => {
-				WA.player.moveTo(672, 1344, 10);
-				}
-			},{
-			label: "CASTING 4",
-			className: "primary",
-			callback: () => {
-				WA.player.moveTo(832, 1344, 10);
-				}
-			},{
-			label: "CASTING 5",
-			className: "primary",
-			callback: () => {
-				WA.player.moveTo(992, 1344, 10);
-				}
-			}]);
+    console.log('Player tags: ',WA.player.tags);
+
+    let buttons = [
+        { label: "TERRASSE", className: "primary", callback: () => { WA.player.moveTo(1248, 672, 10); } },
+        { label: "<br>", className: "primary", callback: () => {} }, // Line break
+        { label: "CASTING 1", className: "primary", callback: () => { WA.player.moveTo(352, 1344, 10); } },
+        { label: "CASTING 2", className: "primary", callback: () => { WA.player.moveTo(512, 1344, 10); } },
+        { label: "<br>", className: "primary", callback: () => {} }, // Line break
+        { label: "CASTING 3", className: "primary", callback: () => { WA.player.moveTo(672, 1344, 10); } },
+        { label: "CASTING 4", className: "primary", callback: () => { WA.player.moveTo(832, 1344, 10); } },
+        { label: "<br>", className: "primary", callback: () => {} }, // Line break
+        { label: "CASTING 5", className: "primary", callback: () => { WA.player.moveTo(992, 1344, 10); } }
+    ];
+
+    currentPopup = WA.ui.openPopup("welcomePopup", "Bienvenue au casting Fram & nxlvl \r \r La présentation se déroulera en visio principalement sur la terrasse \r \r Un membre de la Team va venir vous accueillir \r \r Sinon, pour vous rendre directement sur la terrasse, cliquez sur le premier bouton, pour une salle de casting individuel, cliquez sur un des boutons suivants :", buttons);
+});
     WA.room.area.onLeave('welcome').subscribe(closePopup);
 
 	WA.room.area.onEnter('welcome').subscribe(() => {
