a4f854117d96809469abb1b1ae145160b0d33d89
eneyret <manu@holidee.fr>
2024-09-24 17:39:18 +0200

    PopupCastings

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,7 @@ WA.onInit().then(() => {
 WA.room.area.onLeave('welcome').subscribe(closePopup);
 WA.room.area.onLeave('casting').subscribe(closePopup);
 WA.room.area.onEnter('casting').subscribe(() => {
-		currentPopup = WA.ui.openPopup("welcomePopup", "Vous pouvez patienter ici \r \r Pour vous rendre dans une salle de casting individuel, \r \r cliquez sur un des boutons suivants :", [{
+		currentPopup = WA.ui.openPopup("castingPopup", "Vous pouvez patienter ici \r \r Pour vous rendre dans une salle de casting individuel, \r \r cliquez sur un des boutons suivants :", [{
 			label: "CASTING 1",
 			className: "primary",
 			callback: () => {
@@ -47,23 +47,17 @@ WA.room.area.onEnter('casting').subscribe(() => {
 		}]);
 	});
 WA.room.area.onEnter('welcome').subscribe(() => {
-		currentPopup = WA.ui.openPopup("welcomePopup", "Bienvenue au casting Fram & nxlvl \r \r La présentation se déroulera en visio principalement sur la terrasse \r \r Un membre de la Team va venir vous accueillir \r \r Sinon, pour vous rendre directement sur la terrasse, cliquez sur le premier bouton, pour une salle de casting individuel, cliquez sur un des boutons suivants :", [{
+		currentPopup = WA.ui.openPopup("welcomePopup", "Bienvenue au casting Fram & nxlvl \r \r La présentation se déroulera en visio principalement sur la terrasse \r \r Un membre de la Team va venir vous accueillir \r \r Sinon, pour vous rendre directement sur la terrasse, cliquez sur le premier bouton, pour accéder aux salles de casting individuel, cliquez le second bouton :", [{
 			label: "TERRASSE",
 			className: "primary",
 			callback: () => {
 				WA.player.moveTo(1248, 672, 10);
-			}
+				}
 			},{
-			label: "CASTING 1",
+			label: "CASTINGS",
 			className: "primary",
 			callback: () => {
-				WA.player.moveTo(352, 1344, 10);
-			}
-			},{
-			label: "CASTING 2",
-			className: "primary",
-			callback: () => {
-				WA.player.moveTo(512, 1344, 10);
+				WA.player.moveTo(192, 1312, 10);
 			}
 		}]);
 	});
